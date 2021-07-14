c419fd6a5edd5142edff8db24e14aead1f0f1c97
Umesh91 <annegirikar.umesh@gmail.com>
2021-07-14 15:35:20 +0530

    Add files via upload

---
+++ SpringTestNG/src/main/java/com/springtestNG/SpringBootHelloWorldApplication.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.springtestNG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+/**
+ * @author A631954
+ *
+ */
+@SpringBootApplication
+public class SpringBootHelloWorldApplication 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+
+}
